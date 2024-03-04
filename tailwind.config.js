5465523aef2327dd19ddaf75de8978a4d15de726
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2024-03-04 08:41:52 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./_web/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "space-grotesk": '"Space Grotesk", sans-serif',
+      },
+    },
+
+    screens: {
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
+  },
   // ...
 };
